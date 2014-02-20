31c0ce0d336b40597a42dd0175327220417d51c6
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-02-20 09:32:15 -0600

    add posts_tags migration

--- db/migrate/20140220092447_create_posts.rb
+++ db/migrate/20140220092447_create_posts.rb
@@ -1,4 +1,11 @@
 class CreatePosts < ActiveRecord::Migration
   def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.integer :tag_id
+
+      t.timestamps
+    end
   end
 end
